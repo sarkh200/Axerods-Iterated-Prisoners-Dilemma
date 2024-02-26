d4e38d4674e4a1c26a0ae80bf1a91aa2ba7f098a
sarkh200 <88343505+sarkh200@users.noreply.github.com>
2024-02-26 09:47:49 -0500

    Update Unforgiving.c

--- Strategies/Unforgiving.c
+++ Strategies/Unforgiving.c
@@ -1,6 +1,6 @@
 #include "../IPD.h"
 
-// Cooperates until betrayed, and then defects for the rest of the math
+// Cooperates until betrayed, and then defects for the rest of the match
 __declspec(dllexport) int turn(int *result, int indexOfGame)
 {
     if (indexOfGame != 0 && result[indexOfGame - 1] != 3)
